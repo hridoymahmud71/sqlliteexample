1ef7bddc70324cbe090d29e0738f6995583c3ae5
hridoymahmud71 <hridoymahmud71@gmail.com>
2019-09-24 19:27:54 +0600

    2

---
+++ app/src/main/java/hridoy/aiz/sqliteexample/Data.java
@@ -0,0 +1,19 @@
+package hridoy.aiz.sqliteexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Data extends AppCompatActivity {
+
+    TextView tvData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+
+        tvData = (TextView) findViewById(R.id.tvData);
+    }
+}
--- app/src/main/java/hridoy/aiz/sqliteexample/MainActivity.java
+++ app/src/main/java/hridoy/aiz/sqliteexample/MainActivity.java
@@ -2,13 +2,45 @@ package hridoy.aiz.sqliteexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etName,etCell;
+    Button btnSubmit,btnShowData,btnEditData,btnDeleteData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etCell = (EditText) findViewById(R.id.etCell);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        btnShowData = (Button) findViewById(R.id.btnShowData);
+        btnEditData = (Button) findViewById(R.id.btnEditData);
+        btnDeleteData = (Button) findViewById(R.id.btnDeleteData);
+
+    }
+
+    public void btnSubmit(View v){
+
+    }
+
+    public void btnShowData(View v){
+        startActivity(new Intent(this,Data.class));
+    }
+
+    public void btnEditData(View v){
+
+    }
+
+    public void btnDeleteData(View v){
+
     }
 }
